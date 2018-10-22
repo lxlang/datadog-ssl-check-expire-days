f8156d62292aa655e64ff5e5c6fab2ae9302e28b
Tobias Lang <tlang@fintecsystems.com>
2018-10-22 11:04:27 +0200

    Refactor check.
    Also send ssl.cname status to datadog

--- ssl_check_expire_days.py
+++ ssl_check_expire_days.py
@@ -1,6 +1,3 @@
-# A rewrite of https://workshop.avatarnewyork.com/project/datadog-ssl-expires-check/
-# I prefer to test the site itself instead of the ssl cert file
-
 import time
 import datetime
 import subprocess
@@ -8,19 +5,47 @@ import sys
 from checks import AgentCheck
 
 class SSLCheckExpireDays(AgentCheck):
-    def check(self, instance):
-        metric = "ssl.expire_in_days"
-        site = instance['site']
-        p = subprocess.Popen("echo | openssl s_client -showcerts -servername " + site + " -connect " + site + ":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d\= | xargs -0 -I arg date -d arg \"+%s\"",stdout=subprocess.PIPE, shell=True)
+    def name_matches(self, url):
+        p = subprocess.Popen(
+            "echo | openssl s_client -showcerts -servername " + url + " -connect " + url + ":443 2> /dev/null | openssl x509 -noout -checkhost " + url + "  2> /dev/null ",
+            stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
+
+        if "does match certificate" in output:
+            return True
+
+        return False
+
+    def expire_in_days(self, url):
+        command = "echo | openssl s_client -showcerts -servername " + url + " -connect " + url + ":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d\= | xargs -0 -I arg date -d arg \"+%s\""
+        p = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+
         if output:
             output = output.rstrip("\n")
             d0 = int(time.time())
             d1 = int(output)
             delta = d1 - d0
-            days= delta/24/60/60 # convert the timestamp to days
-            tag="site:" + site # generate the tags
-#            print "metric: " + str(metric) + ", tag: " + str(tag) + ", days: " + str(days)
-            self.gauge(metric, int(days), tags=[tag])
+            valid_days = delta / 24 / 60 / 60  # convert the timestamp to days
+            return int(valid_days)
+
         else:
-            self.gauge(metric, -1, tags=[site])
+            return -1
+
+    def check(self, instance):
+        metric = "ssl.expire_in_days"
+        site = instance['site']
+        tag = "site:" + site
+
+        if not self.name_matches(site):
+            # mark cname check as CRITICAL
+            self.service_check("ssl.cname", status=AgentCheck.CRITICAL, tags=[tag])
+            pass
+
+        else:
+            # mark cname as valid
+            self.service_check("ssl.cname", status=AgentCheck.OK, tags=[tag])
+            days = self.expire_in_days(site)
+            self.gauge(metric, days, tags=[tag])
